6b30e363337a401177feee5e38bf4e1c59516d2d
Chris Eckert <chriseckert.ce@gmail.com>
2020-05-23 22:19:49 +0200

    Add example for useEventListener

--- hooks/useEventListener.js
+++ hooks/useEventListener.js
@@ -32,3 +32,29 @@ function useEventListener(eventName, handler, element = window) {
     [eventName, element] // Re-run if eventName or element changes
   );
 };
+
+// Example
+// // Usage
+// function App() {
+//   // State for storing mouse coordinates
+//   const [coords, setCoords] = useState({ x: 0, y: 0 });
+
+//   // Event handler utilizing useCallback ...
+//   // ... so that reference never changes.
+//   const handler = useCallback(
+//     ({ clientX, clientY }) => {
+//       // Update coordinates
+//       setCoords({ x: clientX, y: clientY });
+//     },
+//     [setCoords]
+//   );
+
+//   // Add event listener using our hook
+//   useEventListener('mousemove', handler);
+
+//   return (
+//     <h1>
+//       The mouse position is ({coords.x}, {coords.y})
+//     </h1>
+//   );
+// }
